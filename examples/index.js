090cbdce0d587e6668a7e05ad9735493a1c26327
Ivan Eisenberg <ivaneisenberg@northwesternmutual.com>
2018-01-22 12:57:38 -0600

    updated example

--- examples/index.js
+++ examples/index.js
@@ -1,44 +1,51 @@
 /* eslint-disable */
 
-import regent from 'regent';
+import regent from '../lib/regent.js';
 
-const { findFirst, or, and, not } = regent.init();
+// initialize (crown) your regent
+const { findFirst, findAll, rule, and, not } = regent.init();
 
-// define your project rules
-export const isHuman = { key: 'species', fn: 'equals', params: ['human'] };
-export const isDog = { key: 'species', fn: 'equals', params: ['canine'] };
-export const olderThan30 = { key: 'vitals.age', fn: 'greaterThan', params: [30] };
+// define your data set
+const data = {
+  name: 'Batman',
+  canFly: false,
+  attributes: {
+    strength: 84,
+    speed: 73,
+  },
+  wearingSuit: true,
+};
 
-// an example of an or relationship
-export const humanOrDog = or([isHuman, isDog]);
+// define your rules
+const wearingSuit = { key: 'wearingSuit', fn: 'equals', params: [true] };
 
-// an example of an and relationship
-export const humanOver30 = and([isHuman, olderThan30]);
+// check a simple rule
+const actLikeBatman = rule(data, wearingSuit) /* ? */ // true
+// returns true
 
-// an example of a negative rule
-export const notHuman = not(isHuman);
 
-// define a logic table, basically an array of objects
-// each object needs a rules key, with an array of valid rules
-const logic = [
-  { greeting: 'Welcome human!', rules: [isHuman] },
-  { greeting: 'Aarf!', rules: [isDog] },
-  { greeting: 'Hello sir.', rules: [humanOver30] },
+// Define another rule
+const strongEnoughToFightJoker = { key: 'attributes.strength', fn: 'greaterThan', params: ['80'] };
+
+// Create a composed rule
+const shouldFightJoker = and([strongEnoughToFightJoker, wearingSuit]);
+
+// Create a not rules
+const notWearingSuit = not(wearingSuit);
+
+// create a logic array
+const actionToTake = [
+  { action: 'Fight Joker', rules: [shouldFightJoker] },
+  { action: 'Go find suit', rules: [notWearingSuit] },
 ];
 
-// you can now use regent to get the proper
-// greeting given a data object.
-const data = {
-  species: 'human',
-  vitals: {
-    age: 34,
-  },
-};
+const action = findFirst(data, actionToTake)
+  .action; /* ? */ // Fight Joker
 
-const appropriateGreeting = findFirst(data, logic).greeting; // "Hello sir."
 
-const dogData = {
-  species: 'dog',
-};
+// findAll
+actionToTake.push({ action: 'Deliver witty one-liner', rules: [wearingSuit, strongEnoughToFightJoker] });
 
-const dogGreeting = findFirst(data, logic).greeting; // "Aarf!"
+const actions = findAll(data, actionToTake)
+  .map(item => item.action) /* ? */ // [ 'Fight Joker', 'Deliver witty one-liner ]
+
