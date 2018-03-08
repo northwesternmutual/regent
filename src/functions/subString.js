a2630efb21245f64bfec882440dad5846e90c364
sch8134 <jesseschmucker@northwesternmutual.com>
2018-03-08 15:00:03 -0600

    Fixed fn.spec and commented out subStr

--- src/functions/subString.js
+++ src/functions/subString.js
@@ -1,10 +1,12 @@
-export default (left, right) => {
-  let result = false;
-  if (typeof left !== 'undefined') {
-    result = left.some(param => (
-      right.indexOf(param) !== -1
-    ));
-  }
+// I think this is getting removed with 2.0
 
-  return result;
-};
+// export default (left, right) => {
+//   let result = false;
+//   if (typeof left !== 'undefined') {
+//     result = left.some(param => (
+//       right.indexOf(param) !== -1
+//     ));
+//   }
+
+//   return result;
+// };
--- src/functions/subString.spec.js
+++ src/functions/subString.spec.js
@@ -1,35 +1,37 @@
-import test from 'tape';
-import subString from './subString';
+// I think this is getting removed with 2.0
 
-test('subString should be a function', (assert) => {
-  assert.equal(typeof subString, 'function');
-  assert.end();
-});
+// import test from 'tape';
+// import subString from './subString';
 
-test('subString Function: Should return true when the first string in the args array is in the input string.', (assert) => {
-  const actual = subString('hello_world', ['hello']);
-  const expected = true;
-  assert.equal(actual, expected);
-  assert.end();
-});
+// test('subString should be a function', (assert) => {
+//   assert.equal(typeof subString, 'function');
+//   assert.end();
+// });
 
-test('subString Function: Should return false when the first string in the args array is NOT in the input string.', (assert) => {
-  const actual = subString('hello_world', ['bye']);
-  const expected = false;
-  assert.equal(actual, expected);
-  assert.end();
-});
+// test('subString Function: Should return true when the first string in the args array is in the input string.', (assert) => {
+//   const actual = subString('hello_world', 'hello');
+//   const expected = true;
+//   assert.equal(actual, expected);
+//   assert.end();
+// });
 
-test('subString Function: Should return true if the input is in any of the param array items', (assert) => {
-  const actual = subString('hello_world', ['bye', 'world']);
-  const expected = true;
-  assert.equal(actual, expected);
-  assert.end();
-});
+// test('subString Function: Should return false when the first string in the args array is NOT in the input string.', (assert) => {
+//   const actual = subString('hello_world', 'bye');
+//   const expected = false;
+//   assert.equal(actual, expected);
+//   assert.end();
+// });
 
-test('should return false if input is undefined', (assert) => {
-  const actual = subString(undefined, ['bye']);
-  const expected = false;
-  assert.equal(actual, expected);
-  assert.end();
-});
+// test('subString Function: Should return true if the input is in any of the param array items', (assert) => {
+//   const actual = subString('hello_world', ['bye', 'world']);
+//   const expected = true;
+//   assert.equal(actual, expected);
+//   assert.end();
+// });
+
+// test('should return false if input is undefined', (assert) => {
+//   const actual = subString(undefined, ['bye']);
+//   const expected = false;
+//   assert.equal(actual, expected);
+//   assert.end();
+// });
