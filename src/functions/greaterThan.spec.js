4dbe2a9c60de405305e54a21bac966b4ee11fa0e
sch8134 <jesseschmucker@northwesternmutual.com>
2018-03-08 14:54:45 -0600

    Fixed some tests that 2.0 changes broke

--- src/functions/greaterThan.spec.js
+++ src/functions/greaterThan.spec.js
@@ -7,15 +7,15 @@ test('greaterThan should be a function', (assert) => {
 });
 
 test('greaterThan should return true if the value given is greater than the param', (assert) => {
-  let actual = greaterThan(1, [0]);
+  let actual = greaterThan(1, 0);
   let expected = true;
   assert.equal(actual, expected);
 
-  actual = greaterThan(4, [3]);
+  actual = greaterThan(4, 3);
   expected = true;
   assert.equal(actual, expected);
 
-  actual = greaterThan(4.2, [4.1]);
+  actual = greaterThan(4.2, 4.1);
   expected = true;
   assert.equal(actual, expected);
 
@@ -23,15 +23,15 @@ test('greaterThan should return true if the value given is greater than the para
 });
 
 test('greaterThan should return false if the value given is less than or equal than the param', (assert) => {
-  let actual = greaterThan(1, [2]);
+  let actual = greaterThan(1, 2);
   let expected = false;
   assert.equal(actual, expected);
 
-  actual = greaterThan(0, [10]);
+  actual = greaterThan(0, 10);
   expected = false;
   assert.equal(actual, expected);
 
-  actual = greaterThan(null, [4]);
+  actual = greaterThan(null, 4);
   expected = false;
   assert.equal(actual, expected);
 
@@ -39,24 +39,18 @@ test('greaterThan should return false if the value given is less than or equal t
 });
 
 test('greaterThan should return false if the key is undefined', (assert) => {
-  const actual = greaterThan(undefined, [0]);
+  const actual = greaterThan(undefined, 0);
   const expected = false;
   assert.equal(actual, expected);
   assert.end();
 });
 
 test('greaterThan should return true for documentation cases', (assert) => {
-  let result = greaterThan(4, [1]); // true
+  let result = greaterThan(4, 1); // true
   assert.equal(result, true, 'case 1');
 
-  result = greaterThan(4, [1, 2, 3]); // true
-  assert.equal(result, true, 'case 2');
-
-  result = greaterThan(4, [5]); // false
+  result = greaterThan(4, 5); // false
   assert.equal(result, false, 'case 3');
-
-  result = greaterThan(4, [1, 2, 3, 4, 5]); // false
-  assert.equal(result, result, 'case 4');
 
   assert.end();
 });
@@ -71,7 +65,7 @@ test('greaterThan should return false if the key is a string that parses to NaN'
     'true',
   ];
   redProps.forEach((val) => {
-    const actual = greaterThan(val, [0]);
+    const actual = greaterThan(val, 0);
     const expected = false;
     assert.equal(actual, expected, `should return false when passed ${val}`);
   });
--- src/functions/numericRange.js
+++ src/functions/numericRange.js
@@ -1,1 +1,7 @@
-export default (left, right) => (left >= right && left <= right);
+export default (left, right) => {
+  if (right.length !== 2) {
+    throw new Error('The right argumrnt to numericRange requires an array of two items.');
+  }
+
+  return (left >= right[0] && left <= right[1]);
+};
