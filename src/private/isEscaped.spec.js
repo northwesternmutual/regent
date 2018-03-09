29ccd9e57f283e62d612ad25a06060d3699363b7
sch8134 <jesseschmucker@northwesternmutual.com>
2018-03-09 10:33:43 -0600

    Wrote the isEscaped specs

---
+++ src/private/isEscaped.spec.js
@@ -0,0 +1,71 @@
+import test from 'tape';
+import isEscaped from './isEscaped';
+
+test('isEscaped Function: Should exist.', (assert) => {
+  const actual = typeof isEscaped;
+  const expected = 'function';
+  assert.equal(actual, expected);
+  assert.end();
+});
+
+test('isEscaped Function: Should return false when the argument does not start with @@', (assert) => {
+  let str = '@hello';
+  let actual = isEscaped(str);
+  const expected = false;
+  assert.equal(actual, expected);
+
+  str = 'hello';
+  actual = isEscaped(str);
+  assert.equal(actual, expected);
+
+  str = 'hello@@';
+  actual = isEscaped(str);
+  assert.equal(actual, expected);
+
+  str = 'he@llo';
+  actual = isEscaped(str);
+  assert.equal(actual, expected);
+
+  assert.end();
+});
+
+test('isEscaped Function: Should return false when the argument is not a string.', (assert) => {
+  let str = {};
+  let actual = isEscaped(str);
+  const expected = false;
+  assert.equal(actual, expected);
+
+  str = [];
+  actual = isEscaped(str);
+  assert.equal(actual, expected);
+
+  str = 5;
+  actual = isEscaped(str);
+  assert.equal(actual, expected);
+
+  str = true;
+  actual = isEscaped(str);
+  assert.equal(actual, expected);
+
+  str = undefined;
+  actual = isEscaped(str);
+  assert.equal(actual, expected);
+
+  assert.end();
+});
+
+test('isEscaped Function: Should return false when the argument does not start with @@', (assert) => {
+  const str = '@hello';
+  const actual = isEscaped(str);
+  const expected = false;
+  assert.equal(actual, expected);
+  assert.end();
+});
+
+test.only('isEscaped Function: Should return true when the argument provided does begin with @@', (assert) => {
+  const str = '@@hello';
+  const actual = isEscaped(str);
+  const expected = true;
+  assert.equal(actual, expected);
+  assert.end();
+});
