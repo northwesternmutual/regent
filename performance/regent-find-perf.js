9b7f7ed6cfb39b03bb69e89bcb48eca658ec7a26
Casey Van Groll <caseyvangroll@northwesternmutual.com>
2019-06-24 04:26:20 -0500

    review MR

--- performance/regent-find-perf.js
+++ performance/regent-find-perf.js
@@ -4,19 +4,19 @@ const regent = require('../lib/regent.js');
 
 const regentFilter = new Benchmark.Suite();
 
-const GREATER_THAN_10 = { left: '@number', fn: 'greaterThan', right: 10 };
-const LESS_THAN_100 = { left: '@number', fn: 'lessThan', right: 100 };
-const BETWEEN_10_AND_100 = regent.and(GREATER_THAN_10, LESS_THAN_100);
+const GREATER_THAN = { left: '@number', fn: 'greaterThan', right: 10 };
+const LESS_THAN = { left: '@number', fn: 'lessThan', right: 100 };
+const BETWEEN = regent.and(GREATER_THAN, LESS_THAN);
 
-const GREATER_THAN_10_FUNCTION = data => data.number > 10;
-const LESS_THAN_100_FUNCTION = data => data.number < 100;
-const BETWEEN_10_AND_100_FUNCTION = data => GREATER_THAN_10_FUNCTION(data) && LESS_THAN_100_FUNCTION(data);
+const GREATER_THAN_FUNCTION = data => data.number > 10;
+const LESS_THAN_FUNCTION = data => data.number < 100;
+const BETWEEN_FUNCTION = data => GREATER_THAN_FUNCTION(data) && LESS_THAN_FUNCTION(data);
 
 const logic = [
-  { value: 'loser', rule: regent.not(LESS_THAN_100) },
-  { value: 'loser', rule: regent.not(GREATER_THAN_10) },
-  { value: 'loser', rule: regent.not(BETWEEN_10_AND_100) },
-  { value: 'winner', rule: BETWEEN_10_AND_100 },
+  { value: 'loser', rule: regent.not(LESS_THAN) },
+  { value: 'loser', rule: regent.not(GREATER_THAN) },
+  { value: 'loser', rule: regent.not(BETWEEN) },
+  { value: 'winner', rule: BETWEEN },
 ];
 
 const data = {
@@ -30,16 +30,16 @@ regentFilter
   })
   .add('Switch Statement using Regent.evaluate', () => {
     switch (true) {
-      case regent.evaluate(regent.not(LESS_THAN_100), data):
+      case regent.evaluate(regent.not(LESS_THAN), data):
         return 'loser';
 
-      case regent.evaluate(regent.not(GREATER_THAN_10), data):
+      case regent.evaluate(regent.not(GREATER_THAN), data):
         return 'loser';
 
-      case regent.evaluate(regent.not(BETWEEN_10_AND_100), data):
+      case regent.evaluate(regent.not(BETWEEN), data):
         return 'loser';
 
-      case regent.evaluate(BETWEEN_10_AND_100, data):
+      case regent.evaluate(BETWEEN, data):
         return 'winner';
 
       default:
@@ -48,16 +48,16 @@ regentFilter
   })
   .add('Switch Statement using pure functions', () => {
     switch (true) {
-      case !LESS_THAN_100_FUNCTION(data):
+      case !LESS_THAN_FUNCTION(data):
         return 'loser';
 
-      case !GREATER_THAN_10_FUNCTION(data):
+      case !GREATER_THAN_FUNCTION(data):
         return 'loser';
 
-      case !BETWEEN_10_AND_100_FUNCTION(data):
+      case !BETWEEN_FUNCTION(data):
         return 'loser';
 
-      case (BETWEEN_10_AND_100_FUNCTION(data)):
+      case (BETWEEN_FUNCTION(data)):
         return 'winner';
 
       default:
