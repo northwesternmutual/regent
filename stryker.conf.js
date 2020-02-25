ca59327f77eab25fdf54d5ed3a978c5b79fb112d
Ivan Eisenberg <ivan.eisenberg@gmail.com>
2020-02-25 12:28:07 -0600

    update gitignore

--- stryker.conf.js
+++ stryker.conf.js
@@ -2,7 +2,7 @@
 // Please see the handbook for more information: https://github.com/stryker-mutator/stryker-handbook/blob/master/stryker/guides/react.md#react
 module.exports = function (config) {
   config.set({
-    mutate: ['src/**/*.js?(x)', '!src/**/*@(.test|.spec|Spec).js?(x)'],
+    mutate: ['src/**/*.js?(x)', '!src/**/*@(.test|.spec|Spec).js?(x)', '!src/constants.js'],
     mutator: 'javascript',
     testRunner: 'jest',
     reporters: ['progress', 'clear-text', 'html'],
