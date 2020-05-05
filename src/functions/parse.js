22396392f4d64db1db2c2ba9867d110628aabf8b
Ivan Eisenberg <ivan.eisenberg@gmail.com>
2020-05-05 17:07:30 -0500

    Closes #39
    Add regent.parse to parse JSON rules syntax
    
    ```javascript
    const json = JSON.stringify({
          fooAndBar: {
            and: [
              { equals: ['@foo', 'foo'] },
              { equals: ['@bar', 'bar'] }
            ]
          },
          emptyFoo: { empty: ['@foo'] },
          fooEqualsFoo: { equals: ['@foo', 'foo'] },
          everyRule: {
            every: ['@arr', { equals: ['@__', 'foo'] }]
          },
          greaterThanOrEqualsRule: { greaterThanOrEquals: ['@foo', 10] },
          greaterThanRule: { greaterThan: ['@foo', 10] },
          lessThanOrEqualsRule: { lessThanOrEquals: ['@foo', 10] },
          lessThanRule: { lessThan: ['@foo', 10] },
          noneRule: {
            none: [
              { equals: ['@foo', 'foo'] },
              { equals: ['@bar', 'bar'] }
            ]
          },
          notRule: {
            not: [
              { equals: ['@foo', 'foo'] }
            ]
          },
          orRule: {
            or: [
              { equals: ['@foo', 'foo'] },
              { equals: ['@foo', 'bar'] }
            ]
          },
          regexRule: {
            regex: ['@foo', '^he[l]lo']
          },
          someRule: {
            some: ['@arr', { equals: ['@__', 'foo'] }]
          },
          typeOfRule: {
            typeOf: ['@foo', 'string']
          },
          xorRule: {
            xor: [
              { equals: ['@foo', 'foo'] },
              { equals: ['@bar', 'bar'] }
            ]
          }
        })
    ```

---
+++ src/functions/parse.js
@@ -0,0 +1,83 @@
+import and from './and'
+import empty from './empty'
+import equals from './equals'
+import every from './every'
+import greaterThanOrEquals from './greater-than-equals'
+import greaterThan from './greater-than'
+import lessThanOrEquals from './less-than-equals'
+import lessThan from './less-than'
+import none from './none'
+import not from './not'
+import or from './or'
+import regex from './regex'
+import some from './some'
+import typeOf from './type-of'
+import xor from './xor'
+
+export function buildRule (jsonRule) {
+  const predicate = Object.keys(jsonRule)[0]
+  switch (predicate) {
+    case 'empty':
+      return empty(...jsonRule[predicate])
+
+    case 'equals':
+      return equals(...jsonRule[predicate])
+
+    case 'greaterThanOrEquals':
+      return greaterThanOrEquals(...jsonRule[predicate])
+
+    case 'greaterThan':
+      return greaterThan(...jsonRule[predicate])
+
+    case 'lessThanOrEquals':
+      return lessThanOrEquals(...jsonRule[predicate])
+
+    case 'lessThan':
+      return lessThan(...jsonRule[predicate])
+
+    case 'regex':
+      // first argument is a key or literal, second is a string
+      // representing a regex that needs to be parsed
+      return regex(jsonRule[predicate][0], new RegExp(jsonRule[predicate][1]))
+
+    case 'typeOf':
+      return typeOf(...jsonRule[predicate])
+
+    // composed rules
+    case 'and':
+      return and(...jsonRule[predicate].map(x => buildRule(x)))
+
+    case 'every':
+      // first argument is the key, the second is a rule to be built
+      return every(jsonRule[predicate][0], buildRule(jsonRule[predicate][1]))
+
+    case 'none':
+      return none(...jsonRule[predicate].map(x => buildRule(x)))
+
+    case 'not':
+      return not(...jsonRule[predicate].map(x => buildRule(x)))
+
+    case 'or':
+      return or(...jsonRule[predicate].map(x => buildRule(x)))
+
+    case 'some':
+      // first argument is the key, the second is a rule to be built
+      return some(jsonRule[predicate][0], buildRule(jsonRule[predicate][1]))
+
+    case 'xor':
+      return xor(...jsonRule[predicate].map(x => buildRule(x)))
+
+    default:
+      return `${predicate} not found`
+  }
+}
+
+export default (json) => {
+  const result = {}
+  const rules = JSON.parse(json)
+  Object.keys(rules).forEach((key) => {
+    result[key] = buildRule(rules[key])
+  })
+
+  return result
+}
