ecb04b046b0320d1763711d77d3ac4af892d8510
Ivan Eisenberg <ivaneisenberg@northwesternmutual.com>
2024-12-04 13:37:59 -0600

    export built in optics, add to sanity test suite

--- example/sanity-test.mjs
+++ example/sanity-test.mjs
@@ -1,7 +1,39 @@
-import { equals, greaterThan, and, or, xor, filter, every, some, not } from '../dist/regent.modern.mjs'
+import {
+  and,
+  divide,
+  empty,
+  equals,
+  every,
+  filter,
+  find,
+  greaterThan,
+  greaterThanOrEquals,
+  lessThan,
+  lessThanOrEquals,
+  make,
+  minus,
+  multiply,
+  none,
+  not,
+  optic,
+  or,
+  parse,
+  plus,
+  predicate,
+  regex,
+  some,
+  typeOf,
+  xor
+} from '../dist/regent.modern.mjs'
+
+const customFunction = (x) => x === 12 || x === 20
+const IS_12_OR_20_MAKE = make(customFunction)
+const IS_12_OR_20_PREDICATE = predicate(customFunction)
+
 
 const FOO_IS_BAR = equals('@foo', 'bar')
 const BIZ_GT_13 = greaterThan('@biz', 13)
+const BIZ_GTOE_13 = greaterThanOrEquals('@biz', 42)
 
 const BAR_AND_GREATER = and(FOO_IS_BAR, BIZ_GT_13)
 const EITHER_OR_RULE = or(FOO_IS_BAR, BIZ_GT_13)
@@ -9,9 +41,12 @@ const EITHER_XOR_RULE = xor(FOO_IS_BAR, BIZ_GT_13)
 
 // In this rule the `__` refers to the current iteration of the `thisWeek` array
 const IS_SUNNY = equals('@__.weatherType', 'sunny')
+const IS_SNOW = equals('@__.weatherType', 'snow')
 const NEXT_THREE_DAYS_ARE_SUNNY = every('@thisWeek', IS_SUNNY, '__')
 const MIGHT_RAIN = equals('@~~.rain', true)
 const CHANCE_OF_RAIN = some('@thisWeek', MIGHT_RAIN, '~~')
+
+const NONE_RULE = none(FOO_IS_BAR, BIZ_GT_13)
 
 const weatherData = {
   thisWeek: [
@@ -23,7 +58,10 @@ const weatherData = {
 
 const data = {
   foo: 'bar',
-  biz: 42
+  biz: 42,
+  items: '',
+  value: 15,
+  month: 'January',
 }
 
 const LOGIC = [
@@ -36,6 +74,35 @@ const STRING_AS_DATA = equals('@[1]', 'b') // access the first element of an arr
 
 const REVERSED_PARAMS = greaterThan(42, '@number')
 
+// Optics
+
+const ADDITION = plus('@temperature', '@temperatureIncrease')
+const DIFFERENCE = minus('@temperature', '@temperatureDecrease')
+const PRODUCT = multiply('@temperature', '@temperatureIncreaseFactor')
+const QUOTIENT = divide('@temperature', '@temperatureDecreaseFactor')
+
+const UNSAFE = greaterThan(ADDITION, 100)
+const UNSAFE_DIFFERENCE = greaterThan(DIFFERENCE, 100)
+const UNSAFE_PRODUCT = greaterThan(PRODUCT, 100)
+const UNSAFE_QUOTIENT = greaterThan(QUOTIENT, 100)
+
+const IS_EMPTY = empty('@items')
+const IS_GREATER = greaterThan('@value', 10)
+const IS_LESS = lessThan('@value', 10)
+const IS_LESS_OR_EQUALS = lessThanOrEquals('@value', 15)
+
+function _minus (left, right) {
+  return left - right
+}
+
+const customMinus = optic(_minus)
+const TEMP = customMinus('@temperature', '@temperatureDrop')
+const FREEZING = lessThanOrEquals(TEMP, 32)
+
+const MONTH_ENDS_WITH_ARY = regex('@month', /ary$/)
+const IS_VALID_TEMPERATURE = typeOf('@biz', 'number')
+
+
 // ========== THUMBS UP TIME ============= //
 
 console.log('FILTER:',
@@ -43,14 +110,36 @@ console.log('FILTER:',
     .map(x => x.text)
     .join(' ') === 'all good!'
     ? '👍'
-    : '👎'
+    : '❌'
 )
-console.log('EQUALS:', FOO_IS_BAR(data) ? '👍' : '👎')
-console.log('GREATER_THAN:', BAR_AND_GREATER(data) ? '👍' : '👎')
-console.log('OR:', EITHER_OR_RULE(data) ? '👍' : '👎')
-console.log('XOR:', EITHER_XOR_RULE(data) ? '👎' : '👍')
-console.log('EVERY:', NEXT_THREE_DAYS_ARE_SUNNY(weatherData) ? '👍' : '👎')
-console.log('SOME && CUSTOM CONTEXT:', CHANCE_OF_RAIN(weatherData) ? '👍' : '👎')
-console.log('STRING AS DATA:', STRING_AS_DATA('abcd') ? '👍' : '👎')
-console.log('REVERSED PARAMS:', REVERSED_PARAMS({ number: 40 }) ? '👍' : '👎')
-console.log('TO_JSON', FOO_IS_BAR.toJson() === '{"equals":["@foo","bar"]}' ? '👍' : '👎')
+
+console.log('FIND:',
+  find(LOGIC, data).text === 'all' ? '👍' : '❌'
+)
+
+console.log('EQUALS:', FOO_IS_BAR(data) ? '👍' : '❌')
+console.log('GREATER_THAN:', BAR_AND_GREATER(data) ? '👍' : '❌')
+console.log('GREATER_THAN_OR_EQUALS:', BIZ_GTOE_13(data) ? '👍' : '❌')
+console.log('OR:', EITHER_OR_RULE(data) ? '👍' : '❌')
+console.log('XOR:', EITHER_XOR_RULE(data) ? '❌' : '👍')
+console.log('EVERY:', NEXT_THREE_DAYS_ARE_SUNNY(weatherData) ? '👍' : '❌')
+console.log('SOME && CUSTOM CONTEXT:', CHANCE_OF_RAIN(weatherData) ? '👍' : '❌')
+console.log('NONE', NONE_RULE({ foo: 'baz', biz: 4 }) ? '👍' : '❌')
+console.log('STRING AS DATA:', STRING_AS_DATA('abcd') ? '👍' : '❌')
+console.log('REVERSED PARAMS:', REVERSED_PARAMS({ number: 40 }) ? '👍' : '❌')
+console.log('TO_JSON', FOO_IS_BAR.toJson() === '{"equals":["@foo","bar"]}' ? '👍' : '❌')
+console.log('OPTIC ADDITION:', UNSAFE({ temperature: 95, temperatureIncrease: 30 }) ? '👍' : '❌')
+console.log('OPTIC SUBTRACTION:', UNSAFE_DIFFERENCE({ temperature: 121, temperatureDecrease: 20 }) ? '👍' : '❌')
+console.log('OPTIC MULTIPLICATION:', UNSAFE_PRODUCT({ temperature: 21, temperatureIncreaseFactor: 5 }) ? '👍' : '❌')
+console.log('OPTIC DIVISION:', UNSAFE_QUOTIENT({ temperature: 400, temperatureDecreaseFactor: 3 }) ? '👍' : '❌')
+console.log('EMPTY:', IS_EMPTY(data) ? '👍' : '❌')
+console.log('GREATER_THAN:', IS_GREATER(data) ? '👍' : '❌')
+console.log('LESS_THAN:', IS_LESS({ value: 5 }) ? '👍' : '❌')
+console.log('LESS_THAN_OR_EQUALS:', IS_LESS_OR_EQUALS({ value: 15 }) ? '👍' : '❌')
+console.log('MAKE', IS_12_OR_20_MAKE(12) ? '👍' : '❌')
+console.log('PREDICATE', IS_12_OR_20_PREDICATE(20) ? '👍' : '❌')
+console.log('TO JSON', BAR_AND_GREATER.toJson() === '{"and":[{"equals":["@foo","bar"]},{"greaterThan":["@biz",13]}]}' ? '👍' : '❌')
+console.log('PARSE', parse('{"fooAndBar": {"and":[{"equals":["@foo","bar"]},{"greaterThan":["@biz",13]}]}}').fooAndBar(data) ? '👍' : '❌')
+console.log('OPTIC', FREEZING({ temperature: 40, temperatureDrop: 8 }) ? '👍' : '❌')
+console.log('REGEX', MONTH_ENDS_WITH_ARY(data) ? '👍' : '❌')
+console.log('TYPEOF', IS_VALID_TEMPERATURE(data) ? '👍' : '❌')
--- src/regent.ts
+++ src/regent.ts
@@ -1,20 +1,24 @@
 import andFn from './functions/and'
+import divideFn from './functions/divide'
 import emptyFn from './functions/empty'
 import equalsFn from './functions/equals'
 import everyFn from './functions/every'
 import filterFn from './functions/filter'
 import findFn from './functions/find'
-import greaterThanOrEqualsFn from './functions/greater-than-equals'
 import greaterThanFn from './functions/greater-than'
-import parseFn from './functions/parse'
-import lessThanOrEqualsFn from './functions/less-than-equals'
+import greaterThanOrEqualsFn from './functions/greater-than-equals'
 import lessThanFn from './functions/less-than'
-import predicateFn from './functions/predicate'
-import opticFn from './functions/optic'
+import lessThanOrEqualsFn from './functions/less-than-equals'
 import makeFn from './functions/make'
+import minusFn from './functions/minus'
+import multiplyFn from './functions/multiply'
 import noneFn from './functions/none'
 import notFn from './functions/not'
+import opticFn from './functions/optic'
 import orFn from './functions/or'
+import parseFn from './functions/parse'
+import plusFn from './functions/plus'
+import predicateFn from './functions/predicate'
 import regexFn from './functions/regex'
 import someFn from './functions/some'
 import typeOfFn from './functions/type-of'
@@ -51,6 +55,19 @@ import xorFn from './functions/xor'
  */
 export const and = andFn
 
+/**
+ * A predicate that returns true if the array is empty
+ *
+ * @param  {FactoryArg} array
+ * @returns boolean
+ * @example
+ * import { empty } from regent
+ *
+ * const IS_EMPTY = empty('@items')
+ *
+ * IS_EMPTY({ items: [] })
+ * // => true
+ */
 export const empty = emptyFn
 
 /**
@@ -88,10 +105,42 @@ export const every = everyFn
 export const filter = filterFn
 export const find = findFn
 export const greaterThanOrEquals = greaterThanOrEqualsFn
+
+/**
+ * A predicate that returns true if the left argument is greater than the right argument
+ *
+ * @param  {FactoryArg} left
+ * @param  {FactoryArg} right
+ * @returns boolean
+ * @example
+ * import { greaterThan } from regent
+ *
+ * const IS_GREATER = greaterThan('@value', 10)
+ *
+ * IS_GREATER({ value: 15 })
+ * // => true
+ */
 export const greaterThan = greaterThanFn
+
 export const parse = parseFn
 export const lessThanOrEquals = lessThanOrEqualsFn
+
+/**
+ * A predicate that returns true if the left argument is less than the right argument
+ *
+ * @param  {FactoryArg} left
+ * @param  {FactoryArg} right
+ * @returns boolean
+ * @example
+ * import { lessThan } from regent
+ *
+ * const IS_LESS = lessThan('@value', 10)
+ *
+ * IS_LESS({ value: 5 })
+ * // => true
+ */
 export const lessThan = lessThanFn
+
 export const predicate = predicateFn
 export const optic = opticFn
 export const make = makeFn
@@ -102,3 +151,7 @@ export const regex = regexFn
 export const some = someFn
 export const typeOf = typeOfFn
 export const xor = xorFn
+export const plus = plusFn
+export const minus = minusFn
+export const multiply = multiplyFn
+export const divide = divideFn
